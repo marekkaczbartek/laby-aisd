1391abc4ec26d6be38782a67745d45e089e54b4b
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2023-06-09 15:03:54 +0200

    added initial templates for matchers

---
+++ src/com/company/matcher/AutomatonMatcher.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class AutomatonMatcher implements IStringMatcher {
+    @Override
+    public List<Integer> validShifts(String textToSearch, String patternToFind) {
+        // TODO: Zaimplementuj algorytm dopasowywania napisów oparty na automacie skończonym
+        return null;
+    }
+}
---
+++ src/com/company/matcher/KMPMatcher.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class KMPMatcher implements IStringMatcher {
+    @Override
+    public List<Integer> validShifts(String textToSearch, String patternToFind) {
+        // TODO: Zaimplementuj algorytm dopasowywania napisów Knutha-Morrisa-Pratta
+        return null;
+    }
+}
